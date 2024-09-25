12872bb329d76314dbce3b2db9c9b955ade0ab50
BZandi <>
2024-09-25 21:47:42 +0200

    Update SPII build configuration and cmake to include dist folder in cleanup step

--- pypupilext/__init__.py
+++ pypupilext/__init__.py
@@ -1,9 +1,23 @@
+from .stereo_calibration import StereoCalibration
+from .single_calibration import SingleCalibration
 import os
 import sys
 
-from .single_calibration import SingleCalibration
-from .stereo_calibration import StereoCalibration
+# Dynamisch site-packages Verzeichnis bestimmen
+site_packages_path = os.path.join(sys.prefix, 'lib', 'python{}.{}'.format(
+    sys.version_info.major, sys.version_info.minor), 'site-packages')
 
+# Sicherstellen, dass site-packages im Pfad ist
+if site_packages_path not in sys.path:
+    sys.path.append(site_packages_path)
+
+# Debug-Ausgabe für den Pfad
+print(f"Current sys.path: {sys.path}")
+print(f"Current __file__ path: {os.path.abspath(__file__)}")
+
+# Importieren der Python-Module
+
+# Versuch, das kompiliertes Modul zu importieren
 try:
     from ._pypupil import *
     print("_pypupil module successfully imported.")
